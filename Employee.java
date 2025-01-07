45eb1b927187909523fe8c1fda72130f532d3713
RITHWIK263 <kyatham.rithwik@gmail.com>
2025-01-07 13:04:11 +0530

    Add files via upload

--- Employee.java
+++ Employee.java
@@ -1,24 +1,32 @@
 public class Employee {
-    public static void main(String[] args) {
-	 int id;
-	 String firstName;
-	 String lastName;
-	 String address;
-	
-	public Employee(int id, String firstName, String lastName, String address) {
-		super();
-		this.id = id;
-		this.firstName = firstName;
-		this.lastName = lastName;
-		this.address = address;
-	}
-	
-	public String getFullName(){
-		return firstName+" "+lastName;
-	}
-	
-	public void sendMessage(){
-		System.out.println("Salary added successfully...");
-	}	
-}
+    private int id;
+    private String firstName;
+    private String lastName;
+    private String address;
 
+    public Employee() {
+        this.id = 1;
+        this.firstName = "Rithwik";
+        this.lastName = "Reddy";
+        this.address = "Nrl";
+    }
+
+    public Employee(int id, String firstName, String lastName, String address) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.address = address;
+    }
+
+    public String getFullName() {
+        return this.firstName + " " + this.lastName;
+    }
+
+    public static void main(String[] args) {
+        Employee emp1 = new Employee();
+        System.out.println("Employee 1: " );
+        System.out.println("ID: " + emp1.id);
+        System.out.println("Name: " + emp1.getFullName());
+        System.out.println("Address: " + emp1.address);
+    }
+}
